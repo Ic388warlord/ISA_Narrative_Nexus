75c06d94e9ff15c0499a78998340d63b6a451ad4
AlexanderLiu <alexanderliu2016@gmail.com>
2023-11-16 22:35:07 -0800

    setup templating

--- gateway-auth-api/src/main.ts
+++ gateway-auth-api/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from "./app.module";
 import { ValidationPipe, VersioningType } from "@nestjs/common";
 import helmet from "helmet";
 import * as cookieParser from "cookie-parser";
+import { join } from "path";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
@@ -22,6 +24,10 @@ async function bootstrap() {
   });
   app.use(helmet());
   app.use(cookieParser());
+
+  app.useStaticAssets(join(__dirname, "..", "public"));
+  app.setBaseViewsDir(join(__dirname, "..", "views"));
+  app.setViewEngine("hbs");
 
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
