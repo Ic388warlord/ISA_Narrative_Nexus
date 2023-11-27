3821104d977a604a58e08eef16ef4f9c67d24ee1
Steven Chow <steven.chow@live.ca>
2023-11-27 13:19:44 -0800

    Add validation

--- gateway-auth-api/src/auth/dtos/loginPostSendToResetPage.dto.ts
+++ gateway-auth-api/src/auth/dtos/loginPostSendToResetPage.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class loginPostBodySendToResetPage {
   @ApiProperty({ type: 'string', default: 'Password', description: 'New user password' })
+  @IsNotEmpty()
   password: string;
 
   @ApiProperty({ type: 'string', default: 'Password', description: 'Reconfirm new user password' })
+  @IsNotEmpty()
   confirmPassword: string;
 }
